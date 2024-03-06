39e1d43220df034e757af360abf74338a1453d0a
Elena Darevsky <elena.darevsky@sap.com>
2024-03-06 17:10:18 +0200

    Render screen

---
+++ src/app/flow-builder/flow-builder.component.ts
@@ -0,0 +1,104 @@
+import {Component, ViewChild} from '@angular/core';
+import {SwdFlowComponent} from '../swd-flow/swd-flow.component';
+import {UiBuilderCustomComponent} from '../ui-builder-custom/ui-builder-custom.component';
+import {BehaviorSubject, tap} from 'rxjs';
+import {AsyncPipe, CommonModule} from '@angular/common';
+import {
+  BarComponent,
+  BarElementDirective,
+  BarLeftDirective,
+  BarMiddleDirective, BarRightDirective,
+  ButtonBarComponent, TitleComponent
+} from '@fundamental-ngx/core';
+import {HttpService} from '../services/http.service';
+
+@Component({
+  selector: 'app-flow-builder',
+  standalone: true,
+  imports: [
+    SwdFlowComponent,
+    UiBuilderCustomComponent,
+    CommonModule,
+    AsyncPipe,
+    BarComponent,
+    BarElementDirective,
+    BarLeftDirective,
+    BarMiddleDirective,
+    ButtonBarComponent,
+    TitleComponent,
+    BarRightDirective
+  ],
+  templateUrl: './flow-builder.component.html',
+  styleUrl: './flow-builder.component.scss'
+})
+export class FlowBuilderComponent {
+  selectedStep$ = new BehaviorSubject<any>(null);
+  openStep$ = new BehaviorSubject<string | null>(null);
+  flowDefinition$ = new BehaviorSubject<any>(null);
+  loadingFinished$ = new BehaviorSubject<any>(false);
+
+  constructor(private httpService: HttpService) {
+    // TODO: unsubscribe
+    this.flowDefinition$.asObservable().pipe(
+      tap(flowDefinition => this.saveFlow(flowDefinition))
+    ).subscribe();
+  }
+
+  get screenDefinition() {
+    // @ts-ignore
+    return this.openStep$.getValue()?.['screenDefinition']
+  }
+
+  get stepId(): any {
+    // @ts-ignore
+    return this.openStep$.getValue()?.['id'];
+  }
+
+  get flowDefinition() {
+    return this.flowDefinition$.getValue();
+  }
+
+  get nodes() {
+    return this.flowDefinition?.nodes;
+  }
+
+  public async ngOnInit() {
+    this.httpService.getFlow().subscribe((res) => {
+      if (res?.flow) {
+        this.flowDefinition$.next(JSON.parse(res.flow));
+      }
+
+      this.loadingFinished$.next(true);
+    });
+  }
+
+  onStepSelected(stepId: any) {
+    this.selectedStep$.next(stepId);
+  }
+
+  onStepOpen(stepId: string) {
+    const node = this.nodes?.find((node: any) => node.id === stepId);
+    this.openStep$.next(node);
+  }
+
+  onScreenUpdated($event: any) {
+    console.log($event);
+    const definition = {...this.flowDefinition};
+    const node = definition?.nodes.find((node: any) => node.id === $event.stepId);
+    node.screenDefinition = $event.screenData;
+    this.flowDefinition$.next(definition);
+  }
+
+  onDefinitionUpdated(definition: any) {
+    this.flowDefinition$.next(definition);
+  }
+
+  saveFlow(definition: any) {
+    if (definition) {
+      this.httpService.setFlow(JSON.stringify(definition)).subscribe((res) => {
+        console.log(res);
+      });
+    }
+
+  }
+}
