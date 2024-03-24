b1af551649652e029665db48ee717d8ca98e7729
Elena Darevsky <elena.darevsky@sap.com>
2024-03-24 16:11:56 +0200

    preserve params

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import {Routes} from '@angular/router';
 import {RunFlowComponent} from './run-flow/run-flow.component';
 import {FlowBuilderComponent} from './flow-builder/flow-builder.component';
-import {UiBuilderCustomComponent} from './ui-builder-custom/ui-builder-custom.component';
-import {VariableDesignerComponent} from './variable-designer/variable-designer.component';
 
 export const routes: Routes = [
   {
     path: 'build-flow', component: FlowBuilderComponent
   },
-  {path: 'run-flow', component: RunFlowComponent},
+  {
+    path: 'run-flow', component: RunFlowComponent
+  },
   {path: '', redirectTo: '/build-flow', pathMatch: 'full'}
 ];
